0f2c5282bc2a2d02753f6e9f4c67681f7b701b03
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-29 17:26:21 -0300

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "instance_ip_addr" {
+  value       = aws_instance.FirstProject.public_ip
+  description = "The public IP address of the main server instance."
+}
+
+output "instance_public_dns" {
+  value       = aws_instance.FirstProject.public_dns
+  description = "The public DNS name of the main server instance."
+}

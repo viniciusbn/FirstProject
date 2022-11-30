7e16117271972b4911d5e371893e212393f25d3b
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-29 22:39:42 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,8 +7,3 @@ output "instance_public_dns" {
   value       = aws_instance.FirstProject.public_dns
   description = "The public DNS name of the main server instance."
 }
-
-output "private_key" {
-  value     = tls_private_key.pk.private_key_pem
-  sensitive = true
-}

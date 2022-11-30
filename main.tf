b5d615615725ee45cf885ab85a7a3a7f1da96e2d
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 15:26:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # The configuration for the `remote` backend.
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "vinibackes"
+#terraform {
+#  backend "remote" {
+#    # The name of your Terraform Cloud organization.
+#    organization = "vinibackes"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "vinibackes_lab"
-    }
-  }
-}
+#    # The name of the Terraform Cloud workspace to store Terraform state files in.
+#    workspaces {
+#      name = "vinibackes_lab"
+#    }
+#  }
+#}
 
 ##PROVIDER 
 provider "aws" {

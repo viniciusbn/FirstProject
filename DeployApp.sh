d015213a0598e9372a51c115198273ebf105fd3e
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 14:43:23 -0300

    Update DeployApp.sh

--- DeployApp.sh
+++ DeployApp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#Install Docker
 sudo apt-get update && sudo apt install docker.io -y
-#Run docker application
 sudo docker pull vbnascimento/firstproject
 sudo docker run -d -p 3000:3000 vbnascimento/firstproject

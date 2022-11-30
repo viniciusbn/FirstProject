a9c1c73d4e608c986af2e20207a55065fab6d0c7
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 14:52:31 -0300

    DeployApp

--- DeployApp.sh
+++ DeployApp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo apt-get update && sudo apt install docker.io -y
-sudo docker pull vbnascimento/firstproject
-sudo docker run -d -p 3000:3000 vbnascimento/firstproject
+sudo apt-get update
+sudo apt install docker.io -y
+sudo docker pull ronaldorigo/mylab:firstproject
+sudo docker run -d -p 3000:3000 ronaldorigo/mylab:firstproject 
+sudo docker pull node

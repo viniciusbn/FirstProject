f1ef63d7df14a49ac80a6439b30f53aab765d0b8
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 10:45:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "tls_private_key" "FirstProject_PK" {
   algorithm = "RSA"
-  rsa_bits  = 4096
+  #rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "FirstProject_KP" {
@@ -26,13 +26,20 @@ resource "aws_key_pair" "FirstProject_KP" {
   public_key = tls_private_key.FirstProject_PK.public_key_openssh
 }
 
+
+
 # An example resource that does nothing.
 resource "aws_instance" "FirstProject" {
   ami                         = "ami-08c40ec9ead489470"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.FirstProject_KP.key_name
   security_groups             = [aws_security_group.FirstProject_SG.name]
-  associate_public_ip_address = true
+  #associate_public_ip_address = true
+  user_data = <<EOF
+  #!/bin/bash
+  sudo apt update && sudo apt install docker.io -y
+  sudo docker run -d -p 3000:3000 vbnascimento/firstproject
+  EOF
   tags = {
     Name = "FirstProject"
   }

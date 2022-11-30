f222ad2336a24ff8f1ce490086f80e426caa0f36
Matheus Andrade <matheusmaais@gmail.com>
2022-11-30 20:08:10 -0300

    tenta ai

--- Backend.tf
+++ Backend.tf
@@ -2,10 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     bucket         = "firstprojectbucketvbn"
-    #dynamodb_table = "FirstProject-tfstate-backend-lock"
-    #profile        = ""
-    #role_arn       = ""
     encrypt        = "true"
-    key            = "FirstProject.tfsate"
+    key            = "FirstProject.tfstate"
   }
 }
--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
-# An example resource.
+variable "tags" {
+  description = "Dynamic tags"
+  type        = map(string)
+  default = {
+    "Name" = "FirstProject"
+    "Terraform" = "true"
+  }
+}
 resource "aws_instance" "FirstProject" {
-  ami                         = "ami-08c40ec9ead489470"
-  instance_type               = "t2.micro"
+  ami                         = data.aws_ami.ubuntu
+  instance_type               = var.instance_type
   key_name                    = aws_key_pair.FirstProject_KP.key_name
   security_groups             = [aws_security_group.FirstProject_SG.name]
   associate_public_ip_address = true
-  user_data                   = <<-EOF
-#!/bin/bash
-set -ex
-sudo apt update && sudo apt install docker.io -y
-sudo docker run -d -p 3000:3000 vbnascimento/firstproject
-EOF
-  tags = {
-    Name = "FirstProject"
+  user_data                   = file("DeployApp.sh")
+
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+    }
   }
 }

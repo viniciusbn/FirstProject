eb5141f8c5805394a4b6f9d0a7e57593c0b24b2e
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 20:34:54 -0300

    Update Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -2,10 +2,10 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     bucket         = "firstprojectbucketvbn"
-    #dynamodb_table = "FirstProject-tfstate-backend-lock"
-    #profile        = ""
-    #role_arn       = ""
+    dynamodb_table = "firstprojectdynamodbvbn"
+    profile        = ""
+    role_arn       = ""
     encrypt        = "true"
-    key            = "FirstProject.tfsate"
+    key            = "FirstProjetc/FirstProject.tfsate"
   }
 }

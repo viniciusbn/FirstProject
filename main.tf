10a17cca3bf639a308428cde9791d91bd7506231
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2022-11-30 11:43:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ resource "aws_key_pair" "FirstProject_KP" {
 
 # An example resource that does nothing.
 resource "aws_instance" "FirstProject" {
-  ami                         = "ami-08c40ec9ead489470"
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.FirstProject_KP.key_name
-  security_groups             = [aws_security_group.FirstProject_SG.name]
+  ami             = "ami-08c40ec9ead489470"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.FirstProject_KP.key_name
+  security_groups = [aws_security_group.FirstProject_SG.name]
   #associate_public_ip_address = true
   user_data = <<-EOF
     #!/bin/bash
